934608a4463a55337e8ce4322058abaf707722a7
Simon Risberg <simon@risberg.eu>
2026-02-15 09:49:23 +0100

    Remove sidebar hover

--- apps/web/src/routes/_app.tsx
+++ apps/web/src/routes/_app.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { SidebarProvider, SidebarInset } from "@metron/ui/components/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
@@ -8,37 +7,9 @@ export const Route = createFileRoute("/_app")({
 });
 
 function AppLayout() {
-  const sidebarRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseEnter = () => {
-    const sidebar = sidebarRef.current?.querySelector(
-      '[data-slot="sidebar"]',
-    ) as HTMLElement | null;
-    if (sidebar?.dataset.state === "collapsed") {
-      sidebar.dataset.collapsible = "";
-      sidebar.dataset.hover = "";
-    }
-  };
-
-  const handleMouseLeave = () => {
-    const sidebar = sidebarRef.current?.querySelector(
-      '[data-slot="sidebar"]',
-    ) as HTMLElement | null;
-    if (sidebar) {
-      sidebar.dataset.collapsible = "icon";
-      delete sidebar.dataset.hover;
-    }
-  };
-
   return (
     <SidebarProvider defaultOpen={false}>
-      <div
-        ref={sidebarRef}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <AppSidebar />
-      </div>
+      <AppSidebar />
       <SidebarInset>
         <main className="flex-1 overflow-auto p-6">
           <Outlet />
